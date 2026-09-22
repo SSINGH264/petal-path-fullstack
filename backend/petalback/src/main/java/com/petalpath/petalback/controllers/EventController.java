c30f28835743491cedae31ece2a289b4deb90973
SSINGH264 <samanta.singhd@gmail.com>
2026-09-22 12:57:46 -0700

    updated the event controller with crud methods

--- backend/petalback/src/main/java/com/petalpath/petalback/controllers/EventController.java
+++ backend/petalback/src/main/java/com/petalpath/petalback/controllers/EventController.java
@@ -1,7 +1,8 @@
 package com.petalpath.petalback.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.petalpath.petalback.models.Event;
+import com.petalpath.petalback.repositories.EventRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,5 +13,41 @@ import java.util.List;
 
 public class EventController {
 
+    private final EventRepository eventRepository;
+
+    public EventController (EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    //Read (getting events) find all
+    @GetMapping
+    public List<Event> getEvents() {
+        return eventRepository.findAll();
+    }
+
+    //Find by id
+    @GetMapping ("/{id}")
+    public Event getItem(@PathVariable int id) {
+        return eventRepository.findById(id).orElse(null);
+    }
+
+    //Adding an item
+    @PostMapping
+    public Event addItem(@RequestBody Event event) {
+        return eventRepository.save(event);
+    }
+
+    //Update item
+    @PutMapping ("/{id}")
+    public Event updateEvent(@PathVariable int id, @RequestBody Event event) {
+        event.setId(id);
+        return eventRepository.save(event);
+    }
+
+    //Delete item
+    @DeleteMapping ("/{id}")
+    public void deleteItem(@PathVariable int id) {
+        eventRepository.deleteById(id);
+    }
 
 }
