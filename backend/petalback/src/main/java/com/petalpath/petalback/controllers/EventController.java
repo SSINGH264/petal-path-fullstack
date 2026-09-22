a0a4649f26148faddf310fd9377f5d77e1dbadda
SSINGH264 <samanta.singhd@gmail.com>
2026-09-22 12:28:28 -0700

    working on event controller

--- backend/petalback/src/main/java/com/petalpath/petalback/controllers/EventController.java
+++ backend/petalback/src/main/java/com/petalpath/petalback/controllers/EventController.java
@@ -1,4 +1,16 @@
 package com.petalpath.petalback.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//Rest for handling HTTP requests
+@RestController
+//Setting the URL
+@RequestMapping ("/events")
+
 public class EventController {
+
+
 }
