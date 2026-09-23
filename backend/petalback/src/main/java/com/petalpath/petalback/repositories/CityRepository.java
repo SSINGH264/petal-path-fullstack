23ad058cad9fe934390063bc9fad62ca7c7ff817
SSINGH264 <samanta.singhd@gmail.com>
2026-09-23 12:54:28 -0700

    added city repository

--- backend/petalback/src/main/java/com/petalpath/petalback/repositories/CityRepository.java
+++ backend/petalback/src/main/java/com/petalpath/petalback/repositories/CityRepository.java
@@ -1,4 +1,7 @@
 package com.petalpath.petalback.repositories;
+import com.petalpath.petalback.models.City;
 
-public interface CityRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository <City, Integer> {
 }
