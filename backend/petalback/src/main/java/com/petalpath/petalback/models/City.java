c3c508cc098b8c962b68d440e34d3420e94594a7
SSINGH264 <samanta.singhd@gmail.com>
2026-09-23 12:42:49 -0700

    added city model with name and state fields

--- backend/petalback/src/main/java/com/petalpath/petalback/models/City.java
+++ backend/petalback/src/main/java/com/petalpath/petalback/models/City.java
@@ -1,4 +1,17 @@
 package com.petalpath.petalback.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // will be mapped to database table
 public class City {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String State;
+
 }
