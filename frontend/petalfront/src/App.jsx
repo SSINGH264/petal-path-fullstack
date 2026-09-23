329436a7bd69ae7afcd45056699694a7c431c8e1
SSINGH264 <samanta.singhd@gmail.com>
2026-09-23 08:29:58 -0700

    added event import

--- frontend/petalfront/src/App.jsx
+++ frontend/petalfront/src/App.jsx
@@ -9,7 +9,7 @@ import SignInForm from "./Components/SignIn/SignIn";
 import SignUp from "./Components/SingUp/SignUp";
 import About from "./Components/About/About";
 import PlantInterest from "./Components/PlantInterest/PlantInterest";
-import Events from "./Components/Events/Events";
+import Events from "./Components/Events/Event";
 
 //Routes for each page
 
